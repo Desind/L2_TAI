b7b0bfd099966967639e6d4fb6007831b6dc75a7
Desind <zaran1998@gmail.com>
2021-03-08 08:55:07 +0100

    navigation through js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let navigation = document.querySelector(".navbar");
+let nav = "<nav>"+"<a href=\"index.html\">Strona główna</a>"+"<a href=\"kontakt.html\">Kontakt</a>"+"</nav>";
+navigation.innerHTML = nav;
+
